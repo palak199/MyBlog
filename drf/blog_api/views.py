29da4dbd5282f482b5d5a2ddcaf7e23aa9dc7a1c
palak199 <p.thapar99@gmail.com>
2021-05-24 20:33:07 +0530

    add permissions

--- drf/blog_api/views.py
+++ drf/blog_api/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser, BasePermission, DjangoModelPermissions
+
+class PostUserWritePermission(BasePermission):
+    message = 'You can\'t edit this one :('
+    
+    def has_obj_permission(self, request, view, obj):
+        
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return obj.author == request.user   
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
